d1fa7f9aa846c6edcdb820948cfc102ad8cca138
wanprabu <wanprabu@gmail.com>
2011-11-04 18:22:45 +0700

    waiting_list.php untuk admin

--- application/controllers/admin/waiting_list.php
+++ application/controllers/admin/waiting_list.php
@@ -28,17 +28,20 @@ class Waiting_list extends CI_Controller {
 		$total_data 	= $this->payment_model->get_total_data();
 		$total_data		= ''.$total_data ;
 		
-		$colModel['no'] 					= array('No',40,TRUE,'center',0);
+		$colModel['no'] 					= array('No',30,TRUE,'center',0);
 		$colModel['KODE_REGISTRASI'] 		= array('Kode Registrasi',110,TRUE,'center',0);
-		$colModel['ATAS_NAMA'] 				= array('Rek . Atas Nama',110,TRUE,'center',1);
-		$colModel['BANK_PENGIRIM'] 			= array('Bank Pengirim',100,TRUE,'center',1);
-		$colModel['JUMLAH_KAMAR'] 			= array('Jumlah Transfer',100,TRUE,'center',1);
-		$colModel['TANGGAL_TRANSFER'] 		= array('Tanggal Transfer',100,FALSE,'center',0);
-		$colModel['BUKTI TRANSFER'] 		= array('Scan Bukti',70,FALSE,'center',0);
-		$colModel['CATATAN'] 				= array('Catatan',150,FALSE,'center',1);
-		$colModel['TIPE_PEMBAYARAN'] 		= array('Jenis Pembayaran',100,TRUE,'center',1);
-		$colModel['TANGGAL_ENTRI']		 	= array('Tgl. Entri',100,FALSE,'center',0);
-		$colModel['TIPE_STATUS']			= array('Status',80,FALSE,'center',1);
+		$colModel['NAMA_USER'] 			= array('Nama User',100,TRUE,'center',1);
+		$colModel['KOTA'] 					= array('Kota',80,TRUE,'center',1);
+		$colModel['EMAIL'] 					= array('Email',150,TRUE,'center',1);
+                $colModel['TELP'] 					= array('Telepon',80,TRUE,'center',0);
+		$colModel['MOBILE'] 				= array('Handphone',80,TRUE,'center',0);
+		$colModel['TANGGAL_PESAN']			= array('Tanggal Pesanan',100,TRUE,'center',1);
+                $colModel['KODE_GROUP']			= array('Group',50,TRUE,'center',1);
+                $colModel['NAMA_PROGRAM']			= array('Program',50,TRUE,'center',1);
+                $colModel['JATUH_TEMPO_UANG_MUKA']			= array('Tgl Jatuh Tempo DP',100,TRUE,'center',1);
+                $colModel['BATAS_WAITING_LIST']			= array('Tgl Batas WL',100,TRUE,'center',0);
+                $colModel['JUMLAH_JAMAAH']			= array('Jumlah Jamaah',80,TRUE,'center',1);
+		$colModel['STATUS']		= array('Status',80,TRUE,'center',1);
 		
 		$gridParams = array(
 		'width' => '1190',
@@ -47,11 +50,11 @@ class Waiting_list extends CI_Controller {
 		'rpOptions' => '[5,10,15,20,25,30,'.$total_data.']',
 		'pagestat' => 'Menampilkan: {from} sampai {to} dari {total} hasil.',
 		'blockOpacity' => 0.5,
-		'title' => 'Data Konfirmasi Pembayaran',
+		'title' => 'Data Daftar Tunggu',
 		'showTableToggleBtn' => false
 		);
 		
-		$grid_js = build_grid_js('flex1',site_url("/admin/konfirmasi/grid_payment/"),$colModel,'no','asc',$gridParams,null);
+		$grid_js = build_grid_js('flex1',site_url("/admin/waiting_list/grid_tunggu/"),$colModel,'no','asc',$gridParams,null);
 		$data['js_grid'] = $grid_js;
 		
 		$data['added_js'] = "";
@@ -60,7 +63,7 @@ class Waiting_list extends CI_Controller {
 		$this->load->view('admin/front',$data);		
 	}
 
-	function grid_payment() 
+	function grid_tunggu()
 	{
 		
 		//call library or helper
@@ -69,12 +72,13 @@ class Waiting_list extends CI_Controller {
 		$this->load->library('form_validation');		
 		
 		//call model here	
-		$this->load->model('payment_model');
+		$this->load->model('waiting_list_model');
 		
-		$valid_fields = array('ATAS_NAMA','BANK_PENGIRIM','JUMLAH_KAMAR', 'CATATAN','TIPE_PEMBAYARAN','TIPE_STATUS');
-		$this->flexigrid->validate_post('ID_PAYMENT','desc',$valid_fields);
+		$valid_fields = array('a.KODE_REGISTRASI','NAMA_USER','EMAIL','KOTA','ALAMAT','TELP','MOBILE','TANGGAL_PESAN','STATUS',
+                    'KODE_GROUP', 'BATAS_WAITING_LIST','JATUH_TEMPO_UANG_MUKA','NAMA_PROGRAM');
+		$this->flexigrid->validate_post('a.ID_ACCOUNT','asc',$valid_fields);
 		
-		$records = $this->payment_model->get_grid_all_payment();
+		$records = $this->waiting_list_model->get_grid_waiting_list();
 		$this->output->set_header($this->config->item('json_header'));
 		
 		$no = 0;
@@ -83,43 +87,22 @@ class Waiting_list extends CI_Controller {
 		{
 			// filter status
 			if($row->STATUS == 0) 
-			{
-				$status = "<a style='cursor:pointer;' onclick=javascript:show_confirm('".site_url()."/admin/konfirmasi/cek_status/".$row->ID_PAYMENT."/".$row->JENIS_PEMBAYARAN."')><font color=\"red\">".$row->TIPE_STATUS."</a></font>";
-			}
-			elseif($row->STATUS == 1) $status = "<font color=\"green\">".$row->TIPE_STATUS."</font>";
-			
-			
-			// view bukti transfer
-			$file = './images/upload/bukti_transfer/'.$row->BUKTI_TRANSFER;
-			if(is_file($file)) 
-			{ 
-				$gambar = '<a href="'.base_url().'/images/upload/bukti_transfer/'.$row->BUKTI_TRANSFER.'"><img src="'.base_url().'/images/flexigrid/book.png"></a>'; 
-			
-			} else { $gambar = "-"; }
-			
-			
-			// filter catatan
-			if($row->CATATAN == 0) { $catatan = "-"; }
-			  else { $catatan = $row->CATATAN; }
-			  
-			
-			// cek format jumlah dolar
-			$jumlah = $this->cek_ribuan($row->JUMLAH_KAMAR)." $";
-			
-		
+				$status = "Registered";
+			elseif($row->STATUS == 1) $status = "Activated";
+                        $jumlah = $row->JUMLAH_ADULT+$row->CHILD_WITH_BED+$row->CHILD_NO_BED+$row->INFANT;
+				
 			// list grid payment
 			$record_items[] = array(
-				$row->ID_PAYMENT,
+				$row->ID_ACCOUNT,
 				$no = $no+1,
 				$row->KODE_REGISTRASI,	
-				$row->ATAS_NAMA,
-				$row->BANK_PENGIRIM,
-				$jumlah,
-				date("d M Y", strtotime($row->TANGGAL_TRANSFER)),
-				$gambar,
-				$catatan,
-				$row->TIPE_PEMBAYARAN,
-				date("d M Y", strtotime($row->TANGGAL_ENTRI)),
+				$row->NAMA_USER,
+                            $row->KOTA, $row->EMAIL, $row->TELP, $row->MOBILE,
+				date("d M Y", strtotime($row->TANGGAL_PESAN)),
+                            $row->KODE_GROUP, $row->NAMA_PROGRAM, 
+                            date("d M Y", strtotime($row->JATUH_TEMPO_UANG_MUKA)),
+				date("d M Y", strtotime($row->BATAS_WAITING_LIST)),
+                            $jumlah,
 				$status
 			);
 		}
@@ -129,101 +112,13 @@ class Waiting_list extends CI_Controller {
 		else
 			$this->output->set_output('{"page":"1","total":"0","rows":[]}');			
 	}
-	
-	
-	function cek_status($id_payment, $jenis_pembayaran)
-	{
-		$this->load->model('payment_model');
-		$this->load->model('packet_model');
-		$this->load->model('program_class_model');
-		$this->load->model('group_departure_model');
-		$this->load->model('jamaah_candidate_model');
-		
-		$data_payment = $this->payment_model->get_all_payment_ByID($id_payment, $jenis_pembayaran);
-		
-		if($data_payment != NULL)
-		{
-			foreach($data_payment->result() as $row)
-			{
-				$id_user_a = $row->ID_ACCOUNT;
-				$kode_reg_a = $row->KODE_REGISTRASI;
-				
-				// CARI DATA PAYMENT YG SUDAH APPROVED
-				$data_payment_app = $this->payment_model->get_payment_byAcc_complete($id_user_a, $kode_reg_a);
-				if($data_payment_app->num_rows() > 0)
-				{
-					$valid = FALSE;
-/*					echo "<script>alert('testing');</script>";
-*/				}else{
-					$valid = TRUE;
-/*					echo "<script>alert('ok');</script>";
-*/				}
-				
-				// UPDATE STATUS PAYMENT MENJADI APPROVED
-				$data_pay = array('STATUS' => 1); 
-				$this->payment_model->update_payment($data_pay, $row->ID_PAYMENT);
-				
-				// UPDATE STATUS PACKET MENJADI 3 = SUDAH PAYMENT
-				$data_packet = $this->packet_model->get_packet_byAcc($id_user_a, $kode_reg_a);
-				foreach($data_packet->result() as $rows)
-				{
-					$id_program = $rows->ID_PROGRAM;
-					$id_group = $rows->ID_GROUP;
-					
-					$data_update = array('STATUS_PESANAN' => 3 ); 
-					$this->packet_model->update_packet($data_update, $rows->ID_PACKET);
-				}
-				
-				// GET DATA MASKAPAI
-				$data_program = $this->program_class_model->get_program($id_program);
-				foreach($data_program->result() as $rows)
-				{
-					$nama_maskapai = $rows->MASKAPAI;
-				}
-				
-				// UPDATE PAGU
-				$data_group = $this->group_departure_model->get_group($id_group);
-				foreach($data_group->result() as $rows)
-				{
-					$pagu_ga = $rows->PAGU_GA;
-					$pagu_sv = $rows->PAGU_SV;
-				}
-				
-				// CARI TOTAL CALON JAMAAH
-				$data_total_jamaah = $this->jamaah_candidate_model->get_total_jamaah($id_user_a, $kode_reg_a);
-				if($data_total_jamaah->num_rows() > 0) { $total_candidate = $data_total_jamaah->num_rows(); }
-				  else { $total_candidate = 0; }
-				
-				if($nama_maskapai == "GARUDA INDONESIA") { $field_group = "PAGU_GA"; $pagu_awal = $pagu_ga; }
-				  elseif($nama_maskapai == "SAUDI AIRLINES") { $field_group = "PAGU_SV"; $pagu_awal = $pagu_sv;}
-				
-				// UPDATE PAGU GROUP
-				if($valid)
-				{
-					if($pagu_ga != 0 || $pagu_sv != 0)
-					{
-						$data_pagu = array($field_group => ($pagu_awal - $total_candidate));
-						$this->group_departure_model->update_group($id_group, $data_pagu);
-					}
-				}
-				
-			}
-			
-			redirect(site_url()."/admin/konfirmasi");
-			
-		} else {
-				echo "<script>alert('Data Tidak Valid');window.location='".site_url()."/admin/konfirmasi';</script>";
-			}
-	}
-	
-	
-	function cek_ribuan($txt)
-	{
-		$pecah = number_format($txt);
-		$ubah = str_replace(",", ".", $pecah);
-		
-		return $ubah;
-	}
+
+        function delete_waiting_list($id_acc, $kode_reg){
+            // delete room packet
+            // delete packet
+            // delete waiting list
+            // send email
+        }
 
         function send_email ($id_acc, $kode_reg, $jenis){
             $this->load->model('accounts_model');
@@ -275,5 +170,5 @@ class Waiting_list extends CI_Controller {
         }
 }//end class
 
-/* End of file /admin/konfirmasi.php */
-/* Location: ./application/controllers/admin/konfirmasi.php */
+/* End of file /admin/waiting_list.php */
+/* Location: ./application/controllers/admin/waiting_list.php */
--- application/models/waiting_list_model.php
+++ application/models/waiting_list_model.php
@@ -5,6 +5,7 @@ class Waiting_list_model extends CI_Model {
 	function __construct()
 	{
 		parent::__construct();
+                $this->CI = get_instance();
 	}
 	
 	function get_all_waiting()
@@ -30,6 +31,31 @@ class Waiting_list_model extends CI_Model {
 
 		return $this->db->get();
 	}
+
+        function get_grid_waiting_list(){
+            $this->db->select('*');
+            $this->db->from('accounts a');
+            $this->db->join('waiting_list w','w.ID_ACCOUNT = a.ID_ACCOUNT AND w.KODE_REGISTRASI = a.KODE_REGISTRASI');
+            $this->db->join('packet p','p.ID_ACCOUNT = a.ID_ACCOUNT AND p.KODE_REGISTRASI = a.KODE_REGISTRASI');
+            $this->db->join('group_departure g','g.ID_GROUP = p.ID_GROUP');
+            $this->db->join('program_class c','c.ID_PROGRAM = p.ID_PROGRAM');
+            $this->db->where('STATUS_PESANAN', 2);
+
+            $this->CI->flexigrid->build_query();
+            $return['records'] = $this->db->get();
+
+            $this->db->select('*');
+            $this->db->from('accounts a');
+            $this->db->join('waiting_list w','w.ID_ACCOUNT = a.ID_ACCOUNT AND w.KODE_REGISTRASI = a.KODE_REGISTRASI');
+            $this->db->join('packet p','p.ID_ACCOUNT = a.ID_ACCOUNT AND p.KODE_REGISTRASI = a.KODE_REGISTRASI');
+            $this->db->join('group_departure g','g.ID_GROUP = p.ID_GROUP');
+            $this->db->join('program_class c','c.ID_PROGRAM = p.ID_PROGRAM');
+            $this->db->where('STATUS_PESANAN', 2);
+            $this->CI->flexigrid->build_query(FALSE);
+            $return['record_count'] = $this->db->count_all_results();
+
+            return $return;
+        }
 	
 	function insert_waiting_list($data){
 		$this->db->trans_begin();
